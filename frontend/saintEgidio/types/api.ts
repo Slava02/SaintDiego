72ab44d2c43c56e252fb05dab805d47af2d293e5
Жувага Вячеслав <zhuvaga.v@wildberries.work>
2025-05-06 12:29:21 +0300

    Removed servers from openapi

--- frontend/saintEgidio/types/api.ts
+++
@@ -1,9 +0,0 @@
-import { TimeSlot, Service, Location, Recurrence } from './event';
-
-export type CreateTimeSlotRequest = Omit<TimeSlot, 'id' | 'location' | 'status'>;
-
-export interface ApiError {
-    code: string;
-    message: string;
-    details?: string;
-} 
