ee92ba07ae9c7c0184568b69665c1a7ca29867c3
Жувага Вячеслав <zhuvaga.v@wildberries.work>
2025-05-07 18:23:49 +0300

    Changed api url

--- frontend/saint-egidio-admin/lib/constants.ts
+++ frontend/saint-egidio-admin/lib/constants.ts
@@ -1,1 +1,3 @@
+console.log('[DEBUG] process.env.NEXT_PUBLIC_API_BASE_URL:', process.env.NEXT_PUBLIC_API_BASE_URL);
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/v1";
+console.log('[DEBUG] API_BASE_URL:', API_BASE_URL); 
